858c7272c093d2dbc0e7c27e56a67661d1b11ffd
Ho Yin Chau <hoyinchau@Hos-MacBook-Pro.local>
2020-05-31 18:27:58 -0700

    Tenenbaum bilinear model paper

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Separating style and content with bilinear models",
+        "excerpt":"In this well-known paper, Tenenbaum and Freeman developed a bilinear model (I will explain bilinear models later) that can separate the “style” and “content” of images. What is meant by “style” and “content”? Using handwritten letters for example, the content would be the letters being written - “a” to “z”...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Separating-Style-and-Content-with-Bilinear-Models/",
+        "teaser":null}]
--- simply_put.md
+++ simply_put.md
@@ -1,6 +1,7 @@
 ---
-layout: page
+layout: posts
 title: Simply Put
 permalink: /simply_put/
 ---
 Neuroscience/machine learning papers, intuitively explained.
+
